46e25b240d08ff7655ed56824baa828a112280ba
Giacomo Munda <giacomo.munda@stud.fh-campuswien.ac.at>
2023-09-16 10:53:56 +0200

    create unit tests for the SemanticSearch class

--- semantic_search.py
+++ semantic_search.py
@@ -1,10 +1,12 @@
+from typing import Union, Optional
 import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer, CrossEncoder
-from typing import Union, Optional
 
 
 class SemanticSearch():
+    '''Class for performing semantic search using SentenceTransformers and Faiss'''
+
     DEFAULT_MODEL_NAME = 'sentence-transformers/multi-qa-MiniLM-L6-cos-v1'
     INDEX_TYPES = {
         "flatL2": faiss.IndexFlatL2,
@@ -12,14 +14,14 @@ class SemanticSearch():
     }
 
     def __init__(
-            self, 
-            embeddings: Union[list, faiss.Index], 
+            self,
+            embeddings: Union[list, faiss.Index],
             model_name: Optional[str] = None,
             index_type: Optional[str] = None,
-            index_file: Optional[str] = None, 
+            index_file: Optional[str] = None,
             cross_encoder_name: Optional[str] = None
     ) -> None:
-        
+
         model_name = model_name or self.DEFAULT_MODEL_NAME
         self.model = SentenceTransformer(model_name)
         if index_file is not None:
@@ -30,21 +32,20 @@ class SemanticSearch():
 
     def create_index(self, embeddings: list, index_type: str) -> faiss.Index:
         docs = embeddings
-        d = embeddings.shape[1]
+        dimension = embeddings.shape[1]
 
         if index_type == "flatL2":
-            index = faiss.IndexFlatL2(d)
+            index = faiss.IndexFlatL2(dimension)
         elif index_type == "flatIP":
-            index = faiss.IndexFlatIP(d) 
+            index = faiss.IndexFlatIP(dimension)
             faiss.normalize_L2(docs)
         else:
             raise ValueError(f"Invalid index type: {index_type}")
-        
+
         index.add(docs.astype("float32"))
-        index.ntotal
 
         return index
-    
+
     def load_index_from_file(self, index_file: str) -> faiss.Index:
         index = faiss.read_index(index_file)
 
@@ -56,31 +57,31 @@ class SemanticSearch():
         return query_embedding
 
     def retrieve_documents(
-            self, 
-            query: str, 
-            text_data: list, 
-            link_data: list, 
-            number_of_documents: int = 10, 
+            self,
+            query: str,
+            text_data: list,
+            link_data: list,
+            number_of_documents: int = 10,
             similarity_threshold: float = 0.3
     ) -> list:
-        
+
         max_documents_to_retrieve = min(number_of_documents, self.index.ntotal)
         query_embedding = self.create_query(query)
         distances, documents_indices = self.index.search(query_embedding, max_documents_to_retrieve)
 
         # sorting the documents based on similarity and adding the links
         retrieved_documents = [
-            (index, text_data[index], distance, link_data[index]) 
-            for distance, index in zip(distances[0], documents_indices[0]) 
+            (index, text_data[index], distance, link_data[index])
+            for distance, index in zip(distances[0], documents_indices[0])
             if distance > similarity_threshold
         ]
-        
+
         if self.cross_encoder is not None:
-            retrieved_documents = self._rerank_documents(query, retrieved_documents)
-        
+            retrieved_documents = self.rerank_documents(query, retrieved_documents)
+
         return retrieved_documents
-    
-    def _rerank_documents(self, query: str, retrieved_documents: list) -> list:
+
+    def rerank_documents(self, query: str, retrieved_documents: list) -> list:
         cross_encoder = self.cross_encoder
         query_doc_pairs = [(query, text) for _, text, _, _ in retrieved_documents]
 
@@ -88,13 +89,12 @@ class SemanticSearch():
         softmax_scores = np.exp(similarity_scores) / np.sum(np.exp(similarity_scores))
 
         reranked_results = [
-            (index, text, score, link) 
+            (index, text, score, link)
             for (index, text, _, link), score in zip(retrieved_documents, softmax_scores)
         ]
         reranked_results = sorted(reranked_results, key=lambda x: x[2], reverse=True)
 
         return reranked_results
-    
+
     def save_index_to_file(self, index_file: str) -> None:
         faiss.write_index(self.index, index_file)
-    
---
+++ test_semantic_search.py
@@ -0,0 +1,63 @@
+from unittest.mock import patch
+import numpy as np
+import pandas as pd
+import pytest
+import faiss
+from semantic_search import SemanticSearch
+
+
+sample_embeddings = np.random.rand(100, 384).astype('float32')
+faiss.normalize_L2(sample_embeddings)
+sample_texts = pd.read_csv("./datasets/papers.csv")["title"][:100].tolist()
+sample_links = pd.read_csv("./datasets/papers.csv")["link"][:100].tolist()
+
+
+@pytest.fixture
+def semantic_search_instance():
+    return SemanticSearch(
+        sample_embeddings,
+        index_type='flatIP',
+        cross_encoder_name='cross-encoder/ms-marco-MiniLM-L-6-v2')
+
+def test_create_query(semantic_search_instance):
+    query = "sample query"
+    query_embedding = semantic_search_instance.create_query(query)
+
+    assert isinstance(query_embedding, np.ndarray)
+    assert query_embedding.shape == (1, 384)
+
+def test_retrieve_documents(semantic_search_instance):
+    query = "sample query"
+    number_of_documents = 4
+
+    assert len(sample_texts) >= number_of_documents
+    assert len(sample_links) >= number_of_documents
+
+    with patch.object(semantic_search_instance, 'cross_encoder', None):
+        results = semantic_search_instance.retrieve_documents(
+            query,
+            sample_texts[:number_of_documents],
+            sample_links[:number_of_documents],
+            number_of_documents=number_of_documents,
+            similarity_threshold=0.3
+        )
+
+    assert isinstance(results, list)
+    for result in results:
+        assert isinstance(result, tuple)
+        assert len(result) == 4  # Ensure each result has 4 elements (index, text, distance, link)
+
+def test_rerank_documents(semantic_search_instance):
+    query = "sample query"
+    results = [
+        (0, "Document 1", 0.5, "Link 1"),
+        (1, "Document 2", 0.4, "Link 2"),
+        (2, "Document 3", 0.6, "Link 3"),
+        (3, "Document 4", 0.2, "Link 4")
+        ]
+    reranked_results = semantic_search_instance.rerank_documents(query, results)
+
+    assert isinstance(reranked_results, list)
+    for reranked_result in reranked_results:
+        assert isinstance(reranked_result, tuple)
+        assert len(reranked_result) == 4
